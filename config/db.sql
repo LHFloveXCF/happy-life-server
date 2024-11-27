86362033e28d64e454bb64ae3bb824267a7a786e
LHFloveXCF <826639991@qq.com>
2024-11-27 23:41:37 +0800

    后台页面：增加玩家表user

--- config/db.sql
+++ config/db.sql
@@ -6,4 +6,13 @@ CREATE TABLE `user_msg` (
   `msg` longtext NOT NULL COMMENT '留言内容',
   `time` bigint(20) NOT NULL COMMENT '发送时间',
   PRIMARY KEY (`use_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+# 用户表
+CREATE TABLE `user` (
+  `user_id` INT NOT NULL AUTO_INCREMENT,
+  `user_name` VARCHAR(45) NOT NULL,
+  `pass_word` VARCHAR(45) NOT NULL,
+  `user_email` VARCHAR(45) NOT NULL,
+  `user_avatar` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
